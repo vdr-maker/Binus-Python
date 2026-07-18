d5d0de3afbb7a28be17157f775bda22977f41449
vdr-maker <davlantisvito@gmail.com@gmail.com>
2026-07-18 17:37:36 +0700

    added volume balok

---
+++ Minggu2/volume_balok.py
@@ -0,0 +1,7 @@
+panjang = float(input("Masukkan Panjang (meter) : "))
+lebar   = float(input("Masukkan Lebar   (meter) : "))
+tinggi  = float(input("Masukkan Tinggi  (meter) : "))
+
+volume = panjang * lebar * tinggi
+
+print(f"  Volume  : {volume:,.4f} m³")
